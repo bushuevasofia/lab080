67ee92d3b489e4561f2b9685814fc90dea7ef5e4
bushuevasofia <bushuevasofia@mail.ru>
2025-06-08 12:11:40 -0400

    add files

---
+++ demo/main.cpp
@@ -0,0 +1,7 @@
+#include <print/print.h>
+#include <iostream>
+
+int main() {
+    print("Hello from Hunter!", std::cout);
+    return 0;
+}
---
+++ src/print.cpp
@@ -0,0 +1,5 @@
+#include "print/print.h"
+
+void print(const std::string& text, std::ostream& out) {
+    out << text << std::endl;
+}
